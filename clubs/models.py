5f7923346d0968eab997bb7a8757e9ea8aa846fa
Gaurav Kulkarni <gkulk007@gmail.com>
2021-04-09 17:11:46 +0530

    updated minor features

--- clubs/models.py
+++ clubs/models.py
@@ -2,21 +2,30 @@ from django.db import models
 
 
 class Club(models.Model):
-    club_name = models.CharField(max_length=255)
-    club_desc = models.TextField(max_length=600)
-    youtube_channel = models.CharField(max_length=300)
-    website = models.CharField(max_length=300)
-    facebook = models.CharField(max_length=400)
-    instagram = models.CharField(max_length=300)
+    club_name = models.CharField(max_length=255, null=True, blank=True)
+    club_desc = models.TextField(max_length=600, null=True, blank=True)
+    youtube_channel = models.CharField(max_length=300, null=True, blank=True)
+    website = models.CharField(max_length=300, null=True, blank=True)
+    facebook = models.CharField(max_length=400, null=True, blank=True)
+    instagram = models.CharField(max_length=300, null=True, blank=True)
+
+    def __str__(self):
+        return self.club_name
 
 
 class Events(models.Model):
     event = models.ForeignKey(Club, on_delete=models.CASCADE)
-    event_title = models.CharField(max_length=255)
-    event_date = models.DateTimeField()
-    yt_link = models.CharField(max_length=255)
+    event_title = models.CharField(max_length=255, null=True, blank=True)
+    event_date = models.DateTimeField(null=True, blank=True)
+    yt_link = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return self.event_title
 
 
 class Members(models.Model):
     member = models.ForeignKey(Club, on_delete=models.CASCADE)
     member_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.member_name
