fba43a80374aae2dfb2290e3d7ac336935971e26
Gaurav Kulkarni <gkulk007@gmail.com>
2021-06-05 23:48:21 +0530

    Upcomming events added to homepage

--- MagE/views.py
+++ MagE/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from clubs import models
+from django.utils import timezone
 
 
 def home(request):
-    return render(request, "home.html")
+
+    now = timezone.now()
+    event_retrive = models.Events.objects.all()
+    upcoming = event_retrive.filter(event_date__gte=now).order_by('event_date')
+    for e in upcoming:
+        print(e)
+    return render(request, "home.html", {'upcoming': upcoming})
